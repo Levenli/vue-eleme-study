4112844980b18922622157ff14ed8e020e6c8434
leven_e <leven_going@163.com>
2020-03-30 01:37:47 +0800

    add baseUrl

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
-
+const urlMap = {
+  development: '/',
+  production: '120.77.25.155/eleme/'
+}
+const baseUrl = urlMap[process.env.NODE_ENV]
 const ERR_OK = 0
 
 export function get (url) {
   return function (params) {
-    return axios.get(url, {
+    return axios.get(baseUrl + url, {
       params
     }).then(res => {
       let { errno, data } = res.data
