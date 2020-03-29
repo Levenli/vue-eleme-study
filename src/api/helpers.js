28d1cc3e9c9af6529fcd8e579b2bced6d1428102
leven_e <leven_going@163.com>
2020-03-30 01:45:04 +0800

    add baseUrl

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const urlMap = {
   development: '/',
-  production: '120.77.25.155/eleme/'
+  production: 'http://120.77.25.155/eleme/'
 }
 const baseUrl = urlMap[process.env.NODE_ENV]
 const ERR_OK = 0
